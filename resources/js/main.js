b86bc2925337c9644a576bd45e883b7ade4c50a4
Yuri Murayama <yuri.murayama@dowjones.com>
2018-02-21 15:57:14 +0900

    First commit

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true
+    pauseOnFocus: false,
+    pauseOnHover: false,
+     // slidesToShow: 1,
+    slidesToScroll: 4
+  });
+  var scrol = 0;
+});
